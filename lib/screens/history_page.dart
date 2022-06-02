071f3499f8082fce3ca8166575cf0957ee3d4d48
Tunmise-Adegoke <mhizsubomi@gmail.com>
2022-06-03 00:44:25 +0100

    changes made

--- lib/screens/history_page.dart
+++ lib/screens/history_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lotto/screens/lotto_drawer.dart';
+import 'package:lotto/utils/constant.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({Key? key}) : super(key: key);
@@ -9,14 +10,22 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kMyBlueColor,
+        elevation: 0,
+      ),
       drawer: LottoDrawer(),
       body: ListView.builder(
-          itemCount: 5,
+          itemCount: 10,
           itemBuilder: (BuildContext context, int index) {
-            return Card();
+            return Container(
+              height: 50,
+              color: Colors.deepPurple,
+            );
           }),
     );
   }
